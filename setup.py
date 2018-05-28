859a0df1174c2416032ee218a3a3a7022ea22569
Konstantin Malanchev <hombit@gmail.com>
2018-05-28 15:36:15 +0300

    Update version to 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gp-multistate-kernel',
-    version='0.0.4',
+    version='0.0.5',
     packages=['multistate_kernel'],
     url='',
     license='',
