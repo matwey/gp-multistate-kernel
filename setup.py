deb4f260d97c9a1ffb44ff1a660ac781a27a6f7f
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2018-06-09 15:18:55 +0300

    Version 0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gp-multistate-kernel',
-    version='0.0.8',
+    version='0.0.9',
     packages=['multistate_kernel'],
     url='',
     license='',
