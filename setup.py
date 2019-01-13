24114dffbadf32372f8b2332e570298f478f42ba
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2019-01-13 19:02:36 +0300

    Version 0.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gp-multistate-kernel',
-    version='0.0.10',
+    version='0.0.11',
     packages=['multistate_kernel'],
     url='',
     license='',
